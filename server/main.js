d452a94198c9fd14889ce633019aab9c07cfb132
Masayuki Sugahara <equaaqua@hotmail.com>
2018-09-30 11:20:31 +0900

    updated api

--- server/main.js
+++ server/main.js
@@ -5,31 +5,13 @@ Meteor.startup(() => {});
 
 Meteor.publish('applause', () => Applause.find());
 
-const Api = new Restivus({
-    enableCors: true,
-    defaultHeaders: {
-        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,HEAD',
-        'Access-Control-Allow-Headers': 'X-CSRF-TOKEN, Content-type'
-    }
-});
+const Api = new Restivus();
 Api.addCollection(Applause, {
     path: 'sounds'
 });
-Api.addRoute('intent', {}, {
+Api.addRoute('intent/:title', {}, {
     post: function() {
-        const param = this.bodyParams;
-        const user = param.args.utterance;
-        const msg = user;
-        return {
-            'bot_id': param.bot_id,
-            'user_id': param.user_id,
-            lang: 'ja-JP',
-            'error_code': 'success',
-            status: 'true',
-            params: {
-                status: 'true',
-                message: msg
-            }
-        };
+        const title = this.urlParams.title;
+        return { updated: Applause.update({ title: title }, { $set: { registeredat: new Date().getTime() } }) };
     }
 });
